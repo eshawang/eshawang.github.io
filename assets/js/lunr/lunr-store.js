7794fb96715ce6c408278da175326d1abe84434e
Esha Wang <esha.wang@gmail.com>
2021-06-30 00:08:10 -0400

    Add static pages.

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,133 @@
+var store = [{
+        "title": "Tech Post 1",
+        "excerpt":"Header   This is tech post 1  ","categories": [],
+        "tags": ["tech"],
+        "url": "/techpost1/",
+        "teaser": null
+      },{
+        "title": "Tech Post 2",
+        "excerpt":"Header   This is tech post 2  ","categories": [],
+        "tags": ["tech"],
+        "url": "/techpost2/",
+        "teaser": null
+      },{
+        "title": "Tech Post 3",
+        "excerpt":"Header   This is tech post 3  ","categories": [],
+        "tags": ["tech"],
+        "url": "/techpost3/",
+        "teaser": null
+      },{
+        "title": "Tech Post 4",
+        "excerpt":"Header   This is tech post 4  ","categories": [],
+        "tags": ["tech"],
+        "url": "/techpost4/",
+        "teaser": null
+      },{
+        "title": "Tech Post 5",
+        "excerpt":"Header   This is tech post 5  ","categories": [],
+        "tags": ["tech"],
+        "url": "/techpost5/",
+        "teaser": null
+      },{
+        "title": "Tech Post 6",
+        "excerpt":"Header   This is tech post 6  ","categories": [],
+        "tags": ["tech"],
+        "url": "/techpost6/",
+        "teaser": null
+      },{
+        "title": "Bougie in NYC",
+        "excerpt":"December 20, 2020 E + K visited D + A in their new apartment in Dumbo, Brooklyn. We ordered delivery from Win Son. Then we took a walk, snapping a classic picture of the Brooklyn bridge. We stopped by One Girl Cookies for some very cutely named desserts (there was...","categories": [],
+        "tags": ["travel"],
+        "url": "/newyork-2020-12-20/",
+        "teaser": null
+      },{
+        "title": "Seattle, Mount Rainier, and Leavenworth",
+        "excerpt":"December 23, 2020 - December 28, 2020 E + K landed in Seattle and drove to Mt. Rainier. We stayed in a small house in the forest with the best stocked fridge of any AirBnB. We were treated to smores, yogurt + granola, bagels, breakfast sandwiches, and more. The first...","categories": [],
+        "tags": ["travel"],
+        "url": "/seattle-2020-12-23/",
+        "teaser": null
+      },{
+        "title": "Photography Post 10",
+        "excerpt":"Header   This is photography post 10  ","categories": [],
+        "tags": ["photography"],
+        "url": "/photographypost10/",
+        "teaser": null
+      },{
+        "title": "Photography Post 2",
+        "excerpt":"Header   This is photography post 2  ","categories": [],
+        "tags": ["photography"],
+        "url": "/photographypost2/",
+        "teaser": null
+      },{
+        "title": "Photography Post 3",
+        "excerpt":"Header   This is photography post 3  ","categories": [],
+        "tags": ["photography"],
+        "url": "/photographypost3/",
+        "teaser": null
+      },{
+        "title": "Photography Post 4",
+        "excerpt":"Header   This is photography post 4  ","categories": [],
+        "tags": ["photography"],
+        "url": "/photographypost4/",
+        "teaser": null
+      },{
+        "title": "Photography Post 5",
+        "excerpt":"Header   This is photography post 5  ","categories": [],
+        "tags": ["photography"],
+        "url": "/photographypost5/",
+        "teaser": null
+      },{
+        "title": "Photography Post 6",
+        "excerpt":"Header   This is photography post 6  ","categories": [],
+        "tags": ["photography"],
+        "url": "/photographypost6/",
+        "teaser": null
+      },{
+        "title": "Photography Post 7",
+        "excerpt":"Header   This is photography post 7  ","categories": [],
+        "tags": ["photography"],
+        "url": "/photographypost7/",
+        "teaser": null
+      },{
+        "title": "Photography Post 8",
+        "excerpt":"Header   This is photography post 8  ","categories": [],
+        "tags": ["photography"],
+        "url": "/photographypost8/",
+        "teaser": null
+      },{
+        "title": "Photography Post 9",
+        "excerpt":"Header   This is photography post 9  ","categories": [],
+        "tags": ["photography"],
+        "url": "/photographypost9/",
+        "teaser": null
+      },{
+        "title": "Photography Post 3",
+        "excerpt":"Header   This is photography post 3  ","categories": [],
+        "tags": ["photography"],
+        "url": "/photographyposttest/",
+        "teaser": null
+      },{
+        "title": "FC Meetup in Norcal",
+        "excerpt":"May 25, 2021 - June 1, 2021 After spending many months at home working, cooking, and playing FFXIV, E + K finally got vaccinated and visited Norcal to see friends and K’s family for the first time since moving to NJ a year ago. S and K’s parents picked us...","categories": [],
+        "tags": ["travel"],
+        "url": "/norcal-2021-05-25/",
+        "teaser": null
+      },{
+        "title": "Weekend in NYC",
+        "excerpt":"June 12, 2021 E + K took the train to 14th street and got their first cronut ever from The Donut Pub. Our original plan was to grab the usual staples from Chelsea Market (lobster roll from The Lobster Place, dumplings from Mokbar Korean Grub), but were enticed by a...","categories": [],
+        "tags": ["travel"],
+        "url": "/newyork-2021-06-12/",
+        "teaser": null
+      },{
+        "title": "Visit to MoMA",
+        "excerpt":"June 18, 2021 On Friday, E finished up a final presentation for her MCL project, and K had all his meetings canceled, so we went to the city for the afternoon. We stopped in at McNulty’s and got some Longjing tea leaves. K found a cute side street called 6-½th...","categories": [],
+        "tags": ["travel"],
+        "url": "/newyork-2021-06-18/",
+        "teaser": null
+      },{
+        "title": "Birthday Weekend",
+        "excerpt":"June 25, 2021 For E’s birthday, K planned an evening out in Greenwich Village. Right off the 9th street station, we visited Té Company for a box of Pineapple Linzer cookies before our dinner reservation at Pinto Garden. K did something embarrassing once again and was punished by E ordering...","categories": [],
+        "tags": ["travel"],
+        "url": "/newyork-2021-06-25/",
+        "teaser": null
+      }]
